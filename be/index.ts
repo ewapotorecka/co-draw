65a0d846af65df2cd07f540f4119d9ef221aaf09
Maciej Bukowski <ma2ciek@gmail.com>
2024-04-27 12:01:24 +0200

    added shared package.json

--- be/index.ts
+++ be/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
